ac1d1629b01ad9b4e4eebb28092d66b76c55c39a
ArvinLleva <arvin.lleva@yahoo.com>
2018-05-19 20:06:15 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -1,1 +1,7 @@
-console.log(name)
+//console.log(name)
+
+/*this loads the index.js file and puts its contents inside of the index variable
+within other_file.js
+the require() function gives access 
+*/
+var index = require("./index.js")
